ef75e122cc3f280df570a794afffabe9e0e61328
jobiols <jorge.obiols@gmail.com>
2017-03-02 08:23:42 -0300

    [ADD] Agregar modulo mrp

--- sams_default/__openerp__.py
+++ sams_default/__openerp__.py
@@ -33,34 +33,36 @@ Customización Sams
 """,
     'author': 'jeo Software',
     'depends': [
-        'l10n_ar_base',             # modulo base para localización argentina
-        'base_vat_unique',          # evita que duplique cuit
-        #'base_vat_unique_parent',  # evita que duplique cuit en multicompañia
-        'disable_openerp_online',   # elimina referencias a odoo online
-        'account_cancel',   # Muestra el check en los diarios que permite cancelar asientos
-#        'product_pricelist_import',# Importa lista de precios y carga productos
-        'hide_product_variants',    # oculta las variantes
-#        'im_chat',                 # mensajeria instantanea entre usuarios de odoo
-#        'express_checkout',        # Facturación express
-        'invoice_order_by_id',      # ordena facturas ultima arriba
-#        'sale_order_recalculate_prices',  # agrega boton para recalcular precios
-#        'consult_product_price',   # consulta de precios
-#        'partner_search',          # permite buscar partners por varios criterios
-#        'account_journal_sequence' # agrega un campo de secuencia en el diario para elegirlos
-         'account_statement_move_import',  # agrega boton de importar aputnes en extractos bancarios
-         'account_journal_sequence', #Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
-        'l10n_ar_aeroo_sale',       # ventas
-        'l10n_ar_aeroo_purchase',   # compras
-        'l10n_ar_aeroo_einvoice',   # factura electrónica
-        'l10n_ar_aeroo_stock',      # stock
-#        'po_custom_reports',       # dependencia requerida
-#        'custom_vat_ledger',       # dependencia requerida
-#        'odoo_argentina_fix',      # patch a la localización
-#        'account_invoice_tax_add', # agrega insercion manual de impuestos para factura de compras
-#        'ticket_citi_fix',         # corrige citi para pv impresor fiscal
-         'product_unique_default_code', # impide que se duplique el default_code
-         'hide_messaging',          # oculta el menu de mensajeria
-         'account_invoice_tax_wizard', #Para cargar percepciones el cual nos remplaza el link de agregar impuestos por otro "agregarimpuestos"
+        'l10n_ar_base',  # modulo base para localización argentina
+        'base_vat_unique',  # evita que duplique cuit
+        # 'base_vat_unique_parent',  # evita que duplique cuit en multicompañia
+        'disable_openerp_online',  # elimina referencias a odoo online
+        'account_cancel',  # Muestra el check en los diarios que permite cancelar asientos
+        #        'product_pricelist_import',# Importa lista de precios y carga productos
+        'hide_product_variants',  # oculta las variantes
+        #        'im_chat',                 # mensajeria instantanea entre usuarios de odoo
+        #        'express_checkout',        # Facturación express
+        'invoice_order_by_id',  # ordena facturas ultima arriba
+        #        'sale_order_recalculate_prices',  # agrega boton para recalcular precios
+        #        'consult_product_price',   # consulta de precios
+        #        'partner_search',          # permite buscar partners por varios criterios
+        #        'account_journal_sequence' # agrega un campo de secuencia en el diario para elegirlos
+        'account_statement_move_import',  # agrega boton de importar aputnes en extractos bancarios
+        'account_journal_sequence', # Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
+        'l10n_ar_aeroo_sale',  # ventas
+        'l10n_ar_aeroo_purchase',  # compras
+        'l10n_ar_aeroo_einvoice',  # factura electrónica
+        'l10n_ar_aeroo_stock',  # stock
+        #        'po_custom_reports',       # dependencia requerida
+        #        'custom_vat_ledger',       # dependencia requerida
+        #        'odoo_argentina_fix',      # patch a la localización
+        #        'account_invoice_tax_add', # agrega insercion manual de impuestos para factura de compras
+        #        'ticket_citi_fix',         # corrige citi para pv impresor fiscal
+        'product_unique_default_code',  # impide que se duplique el default_code
+        'hide_messaging',  # oculta el menu de mensajeria
+        'account_invoice_tax_wizard',
+        # Para cargar percepciones el cual nos remplaza el link de agregar impuestos por otro "agregarimpuestos"
+        'mrp',  # módulo de fabricación
     ],
     'data': [
     ],
