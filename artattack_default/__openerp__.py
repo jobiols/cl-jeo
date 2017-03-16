06a97abefcf237c0f179d8168224ba1b27d562c6
jobiols <jorge.obiols@gmail.com>
2017-03-16 17:24:47 -0300

    [ADD] tres modulos para pos

--- artattack_default/__openerp__.py
+++ artattack_default/__openerp__.py
@@ -47,7 +47,7 @@ Customización Art Attack
         #        'consult_product_price',    # consulta de precios
         #        'partner_search',            # permite buscar partners por varios criterios
         # 'account_journal_sequence'  # agrega un campo de secuencia en el diario para elegirlos
-        # 'account_statement_move_import'  # agrega boton de importar aputnes en extractos bancarios
+        'account_statement_move_import'  # agrega boton de importar aputnes en extractos bancarios
         # 'account_journal_sequence', #Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
         #        'po_custom_reports',        # dependencia requerida
         #        'custom_vat_ledger',        # dependencia requerida
@@ -62,6 +62,7 @@ Customización Art Attack
         'mrp',  # planificacion de producción.
         'pos_pricelist',
         'pos_discount',
+        'pos_order_pricelist_change',
     ],
     'data': [
     ],
