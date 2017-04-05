eb0a07d3eeeb48948efd463b12f6b2e9743a112f
jobiols <jorge.obiols@gmail.com>
2017-04-04 21:07:15 -0300

    [ADD]  account_clean_cancelled_invoice_number

--- power_default/__openerp__.py
+++ power_default/__openerp__.py
@@ -19,53 +19,54 @@
 #
 # -----------------------------------------------------------------------------------
 {
-    'name': 'Art Attack',
+    'name': 'Power Oils',
     'version': '8.0.1.0',
     'category': 'Tools',
-    'summary': 'Customización Art Attack',
+    'summary': 'Customización Power Oils',
     'description': """
 .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-Customización Art Attack
-==========================
+Customización Power Oils
+========================
 """,
-    'author': 'Mario Nuñez, jeo Software',
+    'author': 'jeo Software',
     'depends': [
         'l10n_ar_base',  # modulo base para localización argentina
         'base_vat_unique',  # evita que duplique cuit
         # 'base_vat_unique_parent',  # evita que duplique cuit en multicompañia
         'disable_openerp_online',  # elimina referencias a odoo online
         'account_cancel',  # Muestra el check en los diarios que permite cancelar asientos
-        'account_clean_cancelled_invoice_number',  # permite cancelar facturas (no afip)
+        'sale',      # ventas
+        'purchase',  # compras
         # 'product_pricelist_import',  # Importa lista de precios y carga productos
         # 'hide_product_variants',  # oculta las variantes
         # 'im_chat',  # mensajeria instantanea entre usuarios de odoo
         #        'express_checkout',         # Facturación express
-        # 'invoice_order_by_id',  # ordena facturas ultima arriba
+        #'invoice_order_by_id',  # ordena facturas ultima arriba
         #        'sale_order_recalculate_prices',  # agrega boton para recalcular precios
         #        'consult_product_price',    # consulta de precios
         #        'partner_search',            # permite buscar partners por varios criterios
         # 'account_journal_sequence'  # agrega un campo de secuencia en el diario para elegirlos
-        'account_statement_move_import',  # agrega boton de importar aputnes en extractos bancarios
+        #'account_statement_move_import',  # agrega boton de importar aputnes en extractos bancarios
         # 'account_journal_sequence', #Adds sequence field on account journal and it is going to be considered when choosing journals in differents models.
         #        'po_custom_reports',        # dependencia requerida
         #        'custom_vat_ledger',        # dependencia requerida
         #        'odoo_argentina_fix',       # patch a la localización
         #        'account_invoice_tax_add',  # agrega insercion manual de impuestos para factura de compras
         #        'ticket_citi_fix',          # corrige citi para pv impresor fiscal
-        # 'product_unique_default_code',  # impide que se duplique el default_code
+        'product_unique_default_code',  # impide que se duplique el default_code
         'hide_messaging',  # oculta el menu de mensajeria
-        'base_multi_store',  # agrega capacidad de multitienda analogo a multicompañia
-        # 'voucher_payment_check_fix',  # evita que aparezca cheques propios en medios de pago de cliente
-        'point_of_sale',  # Terminal punto de venta
-        'mrp',  # planificacion de producción.
-        'pos_pricelist',
-        'pos_discount',
-        'pos_order_pricelist_change',
-        'web_pdf_preview',  # vista previa de los pdf antes de imprimir
-        'account_partner_account_summary',  # Imprime el resumen de cuenta de un partner
+        #'base_multi_store',  # agrega capacidad de multitienda analogo a multicompañia
+        #'voucher_payment_check_fix',  # evita que aparezca cheques propios en medios de pago de cliente
+        #'point_of_sale',  # Terminal punto de venta
+        #'mrp',  # planificacion de producción.
+        #'pos_pricelist',
+        #'pos_discount',
+        #'pos_order_pricelist_change',
+        #'web_pdf_preview',  # vista previa de los pdf antes de imprimir
+        #'account_partner_account_summary',  # Imprime el resumen de cuenta de un partner
     ],
     'data': [
     ],
