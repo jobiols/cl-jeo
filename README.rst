93ceb5d277de543f4ddbef099473a03db8d96288
jobiols <jorge.obiols@gmail.com>
2020-07-09 22:23:18 -0300

    [FIX] Version 12.0

---
+++ README.rst
@@ -0,0 +1,2 @@
+CUSTOMIZACION JEO
+=================
---
+++ jeo_default/__manifest__.py
@@ -0,0 +1,79 @@
+# -----------------------------------------------------------------------------------
+#
+#    Copyright (C) 2020  jeo Software  (http://www.jeo-soft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'jeo12',
+    'version': '12.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Customización Jeo Software',
+    'author': 'jeo Software',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+
+    # Here begins odoo-env manifest configuration
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # manifest version, if omitted it is backward compatible
+    'env-ver': '2',
+
+    # if Enterprise it installs in a different directory than community
+    'odoo-license': 'EE',
+
+    # port where odoo starts serving pages
+    'port': '8069',
+
+    # list of url repos to install in the form 'repo-url directory'
+    'git-repos': [
+        'git@github.com:jobiols/cl-jeo.git -b 12.0',
+
+        'https://github.com/jobiols/odoo-addons.git',
+        'https://github.com/ingadhoc/odoo-argentina.git',
+        'https://github.com/ingadhoc/argentina-sale.git',
+        'https://github.com/ingadhoc/sale.git',
+        'https://github.com/ingadhoc/account-financial-tools.git',
+        'https://github.com/ingadhoc/account-payment.git',
+        'https://github.com/ingadhoc/product.git',
+        'https://github.com/ingadhoc/miscellaneous.git',
+        'https://github.com/ingadhoc/argentina-reporting.git',
+        'https://github.com/ingadhoc/reporting-engine.git',
+        'https://github.com/ingadhoc/aeroo_reports.git',
+        'https://github.com/ingadhoc/partner.git',
+        'https://github.com/oca/partner-contact.git',
+        'https://github.com/oca/web.git',
+        'https://github.com/oca/server-tools.git',
+        'https://github.com/oca/social.git',
+        'https://github.com/oca/sale-workflow.git'
+    ],
+
+    # list of images to use in the form 'name image-url'
+    'docker-images': [
+        'odoo jobiols/odoo-ent:12.0e',
+        'postgres postgres:10.1-alpine',
+        'aeroo adhoc/aeroo-docs'
+    ]
+}
